cbdccfa6ac5042f9cfcfa9d8e67f5c99e1006c5e
unknown <119885089+Mats057@users.noreply.github.com>
2023-09-20 19:51:03 -0300

    falta o css

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,44 +1,87 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
-import Tarefa from './Tarefa'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [lista, setLista] = useState([])
-  const [novoItem, setNovoItem] = useState('')
-  function criarTarefa(){
-    if(novoItem === ''){
-      alert('Digite o nome da tarefa')
-      return
+  const listaTarefas = localStorage.getItem("listaTarefas");
+  const [lista, setLista] = useState(listaTarefas ? JSON.parse(listaTarefas) : []);
+  const [novoItem, setNovoItem] = useState("");
+
+
+  useEffect(() => {
+    localStorage.setItem("listaTarefas" , JSON.stringify(lista));
+    }, [lista]);
+
+
+  function criarTarefa() {
+    if (novoItem === "") {
+      alert("Digite o nome da tarefa");
+      return;
     }
-    setLista([...lista, {text: novoItem, isCompleted: false}])
-    setNovoItem('')
-    document.getElementById('nomeTarefa').focus()
-    
+    setLista([...lista, { text: novoItem, isCompleted: false }]);
+    setNovoItem("");
+    document.getElementById("nomeTarefa").focus();
+  }
+
+  function deletar(index) {
+    return () => {
+      const novaLista = [...lista];
+      novaLista.splice(index, 1);
+      setLista(novaLista);
+    };
+  }
+
+  function deleteAll() {
+    return () => {
+      setLista([]);
+    };
+  }
+
+  function selecionada(index){
+    return () => {
+      const novaLista = [...lista];
+      novaLista[index].isCompleted = !novaLista[index].isCompleted;
+      setLista(novaLista);
+    };
   }
 
   return (
     <>
       <h1>Lista de Tarefas</h1>
       <div className="addTarefa" id="container">
-        <input type="text" id='nomeTarefa' name="nomeTarefa" value={novoItem} onChange={(e) => {setNovoItem(e.target.value)}}/>
-        <button onClick={() => criarTarefa()}>
-          Adicionar Tarefa
-        </button>
+        <input
+          type="text"
+          id="nomeTarefa"
+          name="nomeTarefa"
+          value={novoItem}
+          onChange={(e) => {
+            setNovoItem(e.target.value);
+          }}
+        />
+        <button onClick={() => criarTarefa()}>Adicionar Tarefa</button>
       </div>
       <div className="listaTarefas">
-        {
-         lista.length < 1 ?
+        {lista.length < 1 ? (
           <p>Nenhuma tarefa cadastrada</p>
-          :
+        ) : (
           lista.map((item, index) => (
-            <Tarefa key={index} nomeTarefa={item.text} />
+            <div className={item.isCompleted ? "tarefa complete" : "tarefa"}>
+              <p id={index} onClick={selecionada(index)}>{item.text}</p>
+              <button onClick={deletar(index)}>Deletar</button>
+            </div>
           ))
+        )
         }
       </div>
+      <div>
+        {lista.length > 0 ? (
+          <button id="delAll" onClick={deleteAll()}>Deletar todas as tarefas</button>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
